f698101f7b2e4183488418d2ca1ff81bbf2fd1c4
Sidharth Bajaj <w0744581@myscc.ca>
2021-03-15 23:08:00 -0400

    inserted viewpager

--- app/src/main/java/com/example/stayupdated/fragments/ViewPager2Fragment.java
+++ app/src/main/java/com/example/stayupdated/fragments/ViewPager2Fragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.stayupdated.R;
 
@@ -21,10 +23,12 @@ public class ViewPager2Fragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM3 = "param3";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
-    private String mParam2;
+    private int mParam2;
+    private String mParam3;
 
     public ViewPager2Fragment() {
         // Required empty public constructor
@@ -39,11 +43,12 @@ public class ViewPager2Fragment extends Fragment {
      * @return A new instance of fragment ViewPager2Fragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ViewPager2Fragment newInstance(String param1, String param2) {
+    public static ViewPager2Fragment newInstance(String param1, int param2, String param3) {
         ViewPager2Fragment fragment = new ViewPager2Fragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM3, param3);
         fragment.setArguments(args);
         return fragment;
     }
@@ -53,7 +58,8 @@ public class ViewPager2Fragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam2 = getArguments().getInt(ARG_PARAM2);
+            mParam3 = getArguments().getString(ARG_PARAM3);
         }
     }
 
@@ -61,6 +67,22 @@ public class ViewPager2Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_pager2, container, false);
+        View view =  inflater.inflate(R.layout.fragment_view_pager2, container, false);
+/**
+ * assigning values to viewpager
+ */
+        if (mParam1 != null){
+            TextView heading = view.findViewById(R.id.nameCredits);
+            heading.setText(mParam1);
+        }
+        if (mParam2 != 0){
+            ImageView imageView = view.findViewById(R.id.imageCredits);
+            imageView.setImageResource(mParam2);
+        }
+        if (mParam3 != null){
+            TextView description = view.findViewById(R.id.descriptionCredit);
+            description.setText(mParam3);
+        }
+        return view;
     }
 }
